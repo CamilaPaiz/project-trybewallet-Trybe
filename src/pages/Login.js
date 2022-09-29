2f2d861b036ea548890cbf4135ee213fceb661c8
CamilaPaiz <camilapaiz@hotmail.com>
2022-09-29 18:13:53 -0300

    requisito 1 finalizado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { addEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    isDisabled: true,
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => this.verifyInput());
+  };
+
+  verifyInput = () => {
+    const { email, password } = this.state;
+    const emailCharacters = /\S+@\S+\.\S+/;
+    const emailValidation = emailCharacters.test(email);
+    const minPasswordLength = 6;
+    const validPassword = password.length >= minPasswordLength;
+    this.setState({ isDisabled: !(emailValidation && validPassword) });
+  };
+
+  handleClick = () => {
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    dispatch(addEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled } = this.state;
+    return (
+      <div>
+        <label htmlFor="email-input">
+          E-mail
+          <input
+            type="text"
+            name="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha
+          <input
+            type="text"
+            name="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ isDisabled }
+          onClick={ this.handleClick }
+
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
-export default Login;
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,10 @@
-// Coloque aqui suas actions
+// Coloque aqui suas actions-actions type
+
+export const ADD_EMAIL = 'ADD_EMAIL';
+
+// actions creators
+
+export const addEmail = (email) => ({
+  type: ADD_EMAIL,
+  email,
+});
